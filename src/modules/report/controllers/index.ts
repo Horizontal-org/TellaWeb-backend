bea31440fc96bc6e8dac001c106852520665d3c6
Marcos Gutierrez <gmarcos87@gmail.com>
2021-09-09 18:37:05 -0300

    add DeleteByIdReportController

--- src/modules/report/controllers/index.ts
+++ src/modules/report/controllers/index.ts
@@ -1,4 +1,5 @@
 import { CreateReportController } from './create.report.controller';
+import { DeleteByIdReportController } from './delete-by-id.report.controller';
 import { GetByIdReportController } from './get-by-id.report.controller';
 import { ListReportController } from './list.report.controller';
 
@@ -6,4 +7,5 @@ export const reportControllers = [
   CreateReportController,
   GetByIdReportController,
   ListReportController,
+  DeleteByIdReportController,
 ];
