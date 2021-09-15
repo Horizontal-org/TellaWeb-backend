0bb330ad03ece472e3b5e893f3c5ccf2a390db07
Marcos Gutierrez <gmarcos87@gmail.com>
2021-09-15 06:09:27 -0300

    add remoteconfiguration entity

--- src/modules/remoteConfiguration/domain/remote-configuration.entity.ts
+++ src/modules/remoteConfiguration/domain/remote-configuration.entity.ts
@@ -1,7 +1,44 @@
-import { Entity } from 'typeorm';
+import {
+  BeforeInsert,
+  Column,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
 
-import { Exclude } from 'class-transformer';
+import { Exclude, Expose } from 'class-transformer';
 
 @Exclude()
 @Entity()
-export class RemoteConfigurationEntity {}
+export class RemoteConfigurationEntity {
+  @Expose()
+  @PrimaryGeneratedColumn('uuid')
+  id?: string;
+
+  @Expose()
+  @Column()
+  shortCode: string;
+
+  @Expose()
+  @Column()
+  name: string;
+
+  @Expose()
+  @Column('simple-array')
+  applock: boolean[];
+
+  @Expose()
+  @Column('simple-array')
+  camoflage: boolean[];
+
+  @Column({ name: 'created_at' })
+  createdAt!: Date;
+
+  @UpdateDateColumn({ name: 'updated_at' })
+  updatedAt!: Date;
+
+  @BeforeInsert()
+  private beforeInsert(): void {
+    this.createdAt = new Date();
+  }
+}
