d8d14162f78ef1e380c8d8a8cf562ada4562bbdd
Marcos Gutierrez <gmarcos87@gmail.com>
2021-09-17 10:13:30 -0300

    add list controller to remote configuration

--- src/modules/remoteConfiguration/controllers/index.ts
+++ src/modules/remoteConfiguration/controllers/index.ts
@@ -1,5 +1,7 @@
 import { GetByShortCodeRemoteConfigurationController } from './get-by-short-code.remote-configuration.controller';
+import { ListRemoteConfigurationController } from './list.remote-configuration.controller';
 
 export const remoteConfigurationControllers = [
   GetByShortCodeRemoteConfigurationController,
+  ListRemoteConfigurationController,
 ];
