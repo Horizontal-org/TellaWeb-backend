9404c1386cc9f83f19c8daf68e7fe91e21d330ce
Marcos Gutierrez <gmarcos87@gmail.com>
2021-09-17 10:48:38 -0300

    add delete by id controller and providers

--- src/modules/remoteConfiguration/controllers/index.ts
+++ src/modules/remoteConfiguration/controllers/index.ts
@@ -1,7 +1,9 @@
+import { DeleteByIdRemoteConfigurationController } from './delete-by-id.remote-configuration.controller';
 import { GetByShortCodeRemoteConfigurationController } from './get-by-short-code.remote-configuration.controller';
 import { ListRemoteConfigurationController } from './list.remote-configuration.controller';
 
 export const remoteConfigurationControllers = [
   GetByShortCodeRemoteConfigurationController,
   ListRemoteConfigurationController,
+  DeleteByIdRemoteConfigurationController,
 ];
--- src/modules/remoteConfiguration/remote-configuration.providers.ts
+++ src/modules/remoteConfiguration/remote-configuration.providers.ts
@@ -3,15 +3,22 @@ import { GetByShortCodeRemoteConfigurationApplication } from './application';
 import { GetByShortCodeRemoteConfigurationService } from './services';
 import { ListRemoteConfigurationApplication } from './application/list.remote-configuration.application';
 import { ListRemoteConfigurationService } from './services/list.remote-configuration.service';
+import { DeleteByIdRemoteConfigurationService } from './services/delete-by-id.remote-configuration.service';
+import { DeleteByIdRemoteConfigurationApplication } from './application/delete-by-id.remote-configuration.application';
 
 export const getByShortCodeRemoteConfigurationApplicationProvider = {
   provide: TYPES.applications.IGetByShortCodeRemoteConfigurationApplication,
   useClass: GetByShortCodeRemoteConfigurationApplication,
 };
 
-export const listRemoteConfiguratioApplicationProvider = {
+export const listRemoteConfigurationApplicationProvider = {
   provide: TYPES.applications.IListRemoteConfigurationApplication,
   useClass: ListRemoteConfigurationApplication,
+};
+
+export const deleteByIdRemoteConfigurationApplicationProvider = {
+  provide: TYPES.applications.IDeleteByIdRemoteConfigurationApplication,
+  useClass: DeleteByIdRemoteConfigurationApplication,
 };
 
 export const getByShortCodeRemoteConfigurationServiceProvider = {
@@ -24,12 +31,19 @@ export const listRemoteConfigurationServiceProvider = {
   useClass: ListRemoteConfigurationService,
 };
 
+export const deleteByIdRemoteConfigurationServiceProvider = {
+  provide: TYPES.services.IDeleteByIdRemoteConfigurationService,
+  useClass: DeleteByIdRemoteConfigurationService,
+};
+
 export const applicationsRemoteConfigurationProviders = [
   getByShortCodeRemoteConfigurationApplicationProvider,
-  listRemoteConfiguratioApplicationProvider,
+  listRemoteConfigurationApplicationProvider,
+  deleteByIdRemoteConfigurationApplicationProvider,
 ];
 
 export const servicesRemoteConfigurationProviders = [
   getByShortCodeRemoteConfigurationServiceProvider,
   listRemoteConfigurationServiceProvider,
+  deleteByIdRemoteConfigurationServiceProvider,
 ];
