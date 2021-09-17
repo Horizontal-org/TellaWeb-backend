712485342f7b9b93ec37780e891d1caf1402f80f
Marcos Gutierrez <gmarcos87@gmail.com>
2021-09-17 10:17:41 -0300

    inject remote configuration list service and application

--- src/modules/remoteConfiguration/remote-configuration.providers.ts
+++ src/modules/remoteConfiguration/remote-configuration.providers.ts
@@ -1,10 +1,17 @@
 import { TYPES } from './interfaces';
 import { GetByShortCodeRemoteConfigurationApplication } from './application';
 import { GetByShortCodeRemoteConfigurationService } from './services';
+import { ListRemoteConfigurationApplication } from './application/list.remote-configuration.application';
+import { ListRemoteConfigurationService } from './services/list.remote-configuration.service';
 
 export const getByShortCodeRemoteConfigurationApplicationProvider = {
   provide: TYPES.applications.IGetByShortCodeRemoteConfigurationApplication,
   useClass: GetByShortCodeRemoteConfigurationApplication,
+};
+
+export const listRemoteConfiguratioApplicationProvider = {
+  provide: TYPES.applications.IListRemoteConfigurationApplication,
+  useClass: ListRemoteConfigurationApplication,
 };
 
 export const getByShortCodeRemoteConfigurationServiceProvider = {
@@ -12,10 +19,17 @@ export const getByShortCodeRemoteConfigurationServiceProvider = {
   useClass: GetByShortCodeRemoteConfigurationService,
 };
 
+export const listRemoteConfigurationServiceProvider = {
+  provide: TYPES.services.IListRemoteConfigurationService,
+  useClass: ListRemoteConfigurationService,
+};
+
 export const applicationsRemoteConfigurationProviders = [
   getByShortCodeRemoteConfigurationApplicationProvider,
+  listRemoteConfiguratioApplicationProvider,
 ];
 
 export const servicesRemoteConfigurationProviders = [
   getByShortCodeRemoteConfigurationServiceProvider,
+  listRemoteConfigurationServiceProvider,
 ];
