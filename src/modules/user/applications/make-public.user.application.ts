a2c9fde1255cc51eed005a2743b95345350efe2c
Marcos Gutierrez <gmarcos87@gmail.com>
2021-09-18 12:15:46 -0300

    admin cant be updated as public

--- src/modules/user/applications/make-public.user.application.ts
+++ src/modules/user/applications/make-public.user.application.ts
@@ -1,8 +1,10 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { classToPlain, plainToClass } from 'class-transformer';
 import { hashPassword } from 'common/utils/password.utils';
+import { RolesUser } from '../domain';
 
 import { EditUserDto, ReadUserDto } from '../dto';
+import { AdminCantBePublicUserException } from '../exceptions/admin-not-public.user.exception';
 import {
   TYPES,
   IFindByUsernameUserService,
@@ -25,12 +27,16 @@ export class MakePublicUserApplication implements IMakePublicUserApplication {
   async execute(username: string): Promise<ReadUserDto> {
     try {
       const user = await this.findByUsernameUserService.execute(username);
+      if (user.role === RolesUser.ADMIN)
+        throw new AdminCantBePublicUserException();
       user.password = await hashPassword(username);
       const updatedUser = await this.editUserService.execute(
         plainToClass(EditUserDto, user),
       );
       return plainToClass(ReadUserDto, updatedUser);
-    } catch (_) {
+    } catch (error) {
+      if (error instanceof AdminCantBePublicUserException) throw error;
+
       const userCreated = this.createUserService.execute({
         username,
         password: username,
