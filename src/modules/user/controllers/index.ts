8f3cdb6dfe48d9c032b65809e25f77c707471450
Marcos Gutierrez <gmarcos87@gmail.com>
2021-11-30 06:25:29 +0100

    change user password

--- src/modules/user/controllers/index.ts
+++ src/modules/user/controllers/index.ts
@@ -2,8 +2,10 @@ import { EditUserController } from './edit.user.controller';
 import { CreateUserController } from './create.user.controller';
 import { GetProfileUserController } from './get-profile.user.controller';
 import { GetByUsernameController } from './get-by-username.controller';
+import { ChangePasswordUserController } from './change-password.user.controller';
 
 export const userControllers = [
+  ChangePasswordUserController,
   CreateUserController,
   EditUserController,
   GetProfileUserController,
--- src/modules/user/dto/index.ts
+++ src/modules/user/dto/index.ts
@@ -3,3 +3,4 @@ export { ChangeRoleUserDto } from './change-role.user.dto';
 export { ReadUserDto } from './read.user.dto';
 export { CredentialUserDto } from './credential.user.dto';
 export { EditUserDto } from './edit.user.dto';
+export { ChangePasswordUserDto } from './change-password.user.dto';
