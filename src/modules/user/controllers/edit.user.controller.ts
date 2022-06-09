b419b7d9193bc2ee77c6418807e2b777c75d1213
Marcos Gutierrez <gmarcos87@gmail.com>
2022-06-09 16:46:00 +0200

    add example

--- src/modules/user/controllers/edit.user.controller.ts
+++ src/modules/user/controllers/edit.user.controller.ts
@@ -1,17 +1,20 @@
+import { ForbiddenError } from '@casl/ability';
 import { Body, Inject, Param, ParseUUIDPipe, Post } from '@nestjs/common';
 import { ApiResponse } from '@nestjs/swagger';
+import { AbilityFactory, Actions } from 'casl/casl-ability.factory';
 
 import { AuthController } from 'common/decorators/auth-controller.decorator';
+import { LoggedUser } from 'modules/auth/decorators';
 
-import { RolesUser } from '../domain';
 import { EditUserDto, ReadUserDto } from '../dto';
 import { IEditUserApplication, TYPES } from '../interfaces';
 
-@AuthController('user', [RolesUser.ADMIN])
+@AuthController('user')
 export class EditUserController {
   constructor(
     @Inject(TYPES.applications.IEditUserApplication)
     private readonly editUserApplication: IEditUserApplication,
+    private readonly abilityFactory: AbilityFactory,
   ) {}
 
   @ApiResponse({ type: ReadUserDto })
@@ -19,8 +22,17 @@ export class EditUserController {
   async handler(
     @Body() editUserDto: Partial<EditUserDto>,
     @Param('userId', new ParseUUIDPipe()) userId: string,
+    @LoggedUser() loggedUser: ReadUserDto,
   ): Promise<ReadUserDto> {
     editUserDto.id = userId;
+
+    const ability = this.abilityFactory.defineAbility(loggedUser);
+
+    ForbiddenError.from(ability).throwUnlessCan(
+      Actions.Update,
+      Object.assign(new ReadUserDto(), editUserDto),
+    );
+
     const user = await this.editUserApplication.execute(editUserDto);
 
     return user;
