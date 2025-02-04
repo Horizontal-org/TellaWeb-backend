312173b085677bdce4b77327efc70019053effed
juan <juandans01@gmail.com>
2025-02-04 17:56:43 -0300

    fix backup download link

--- src/modules/backup/controllers/download.backup.controller.ts
+++ src/modules/backup/controllers/download.backup.controller.ts
@@ -1,4 +1,4 @@
-import { Get, Header, Inject, Param, Res } from '@nestjs/common';
+import { Controller, Get, Header, Inject, Param, Res } from '@nestjs/common';
 import { AuthController } from 'common/decorators/auth-controller.decorator';
 import { Response } from 'express';
 import { JwtTypes } from 'modules/jwt/domain/jwt-types.auth.enum';
@@ -6,7 +6,7 @@ import { RolesUser } from 'modules/user/domain';
 
 import { IDownloadBackupService, TYPES } from '../interfaces';
 
-@AuthController('backup', [RolesUser.ADMIN], JwtTypes.WEB)
+@AuthController('backup', [RolesUser.ADMIN], JwtTypes.ALL)
 export class DownloadBackupController {
   constructor(
     @Inject(TYPES.services.IDownloadBackupService)
@@ -20,10 +20,10 @@ export class DownloadBackupController {
     backupId: string,
     @Res() res: Response,
   ) {
-    console.log('on downbload')
     const zipStream = await this.downloadBackupService.execute(
         backupId,
     );
+    res.header('Content-disposition', 'attachment; filename=TELLAWEB_BACKUP.zip');
     zipStream.pipe(res);
   }
 }
